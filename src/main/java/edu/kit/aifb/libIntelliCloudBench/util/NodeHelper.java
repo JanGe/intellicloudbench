1c6ef75bf56bc5ada5161db61be11fcc3cf7c873
Jan Gerlinger <jan.gerlinger@gmx.de>
2012-10-10 00:29:51 +0200

    Closing SSH channels correctly

--- src/main/java/edu/kit/aifb/libIntelliCloudBench/util/NodeHelper.java
+++ src/main/java/edu/kit/aifb/libIntelliCloudBench/util/NodeHelper.java
@@ -1,32 +1,32 @@
 /*
-* This file is part of libIntelliCloudBench.
-*
-* Copyright (c) 2012, Jan Gerlinger <jan.gerlinger@gmx.de>
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-* * Redistributions of source code must retain the above copyright
-* notice, this list of conditions and the following disclaimer.
-* * Redistributions in binary form must reproduce the above copyright
-* notice, this list of conditions and the following disclaimer in the
-* documentation and/or other materials provided with the distribution.
-* * Neither the name of the Institute of Applied Informatics and Formal
-* Description Methods (AIFB) nor the names of its contributors may be used to
-* endorse or promote products derived from this software without specific prior
-* written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * This file is part of libIntelliCloudBench.
+ *
+ * Copyright (c) 2012, Jan Gerlinger <jan.gerlinger@gmx.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the Institute of Applied Informatics and Formal
+ * Description Methods (AIFB) nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior
+ * written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 package edu.kit.aifb.libIntelliCloudBench.util;
 
@@ -77,22 +77,38 @@ public class NodeHelper {
 	    throws RunScriptOnMachineException {
 		runner.log("$ " + command);
 
-		ExecChannel channel = ssh.execChannel(command);
+		String output = "";
+		
+		ExecChannel channel = null;
+		try {
+			channel = ssh.execChannel(command);
 
-		if (input != null) {
-			try {
-				setInput(runner, input, channel);
-			} catch (IOException e) {
-				throw new RunScriptOnMachineException(Integer.MAX_VALUE, command, "Error when providing interactive options:\n"
-				    + input);
+			if (input != null) {
+				try {
+					setInput(runner, input, channel);
+				} catch (IOException e) {
+					throw new RunScriptOnMachineException(
+					    Integer.MAX_VALUE,
+					    command,
+					    "Error when providing interactive options:\n" + input);
+				}
 			}
+
+			output = getOutput(runner, command, channel);
+
+			if (channel.getExitStatus() != null) {
+				logErrorOutput(runner, command, channel, output);
+			}
+
+		} finally {
+			if (channel != null)
+				try {
+					channel.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 		}
 
-		String output = getOutput(runner, command, channel);
-
-		if (channel.getExitStatus() != null) {
-			logErrorOutput(runner, command, channel, output);
-		}
 		return output;
 	}
 
@@ -143,10 +159,12 @@ public class NodeHelper {
 				runner.appendToLog(line);
 				sb.append(line);
 				sb.append("\n");
-//				/* As PTS doesn't return useful status codes, we have to do some additional checks here */
-//				if (line.equals("The following tests failed to install:")) {
-//					throw new RunScriptOnMachineException(0, command, "Benchmark could not be installed:\n" + sb.toString());
-//				}
+				// /* As PTS doesn't return useful status codes, we have to do some
+				// additional checks here */
+				// if (line.equals("The following tests failed to install:")) {
+				// throw new RunScriptOnMachineException(0, command,
+				// "Benchmark could not be installed:\n" + sb.toString());
+				// }
 			}
 		} catch (IOException e) {
 			try {
